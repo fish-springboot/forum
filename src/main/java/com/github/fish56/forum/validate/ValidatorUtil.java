9a3b1bd773cb9ef41f823fde34719b37e14cad92
bitfishxyz <bitfishxyz@gmail.com>
2019-05-28 17:19:11 +0800

    添加文档说明

--- src/main/java/com/github/fish56/forum/validate/ValidatorUtil.java
+++ src/main/java/com/github/fish56/forum/validate/ValidatorUtil.java
@@ -1,19 +1,12 @@
 package com.github.fish56.forum.validate;
 
-import com.github.fish56.forum.service.ServerResponseMessage;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.stereotype.Component;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.validation.beanvalidation.SpringValidatorAdapter;
 
 import javax.validation.Validation;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
 
 /**
  * 手动检验字段的合法性
